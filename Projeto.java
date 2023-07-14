e84c56bfdbdbc781f4f2735cb8f705beab1b9a01
Lucas <lucasamorim.slv@gmail.com>
2023-07-14 11:41:24 -0300

    Criation of repositorie

---
+++ Projeto.java
@@ -0,0 +1,4 @@
+/* 
+    *O programa tem como objetivo integrar o banco de dados com a catraca de leitor biométrico fazendo, assim, o controle e monitoramento do acesso de um estabelecimento
+    
+*/
